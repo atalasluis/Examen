f6886ba2b1267601e8562b6fa61d9a0731cf5670
LuisSalazar <atalasturey@gmail.com>
2021-05-25 10:24:58 -0400

    primera prueva rspec 2

--- spec/cambio.rb
+++ spec/cambio.rb
@@ -3,4 +3,7 @@ RSpec.describe "debuelve un areglo con el cambio" do
     it"deveria devolver un arreglo "do
         expect(Cambio(19)).to eq([19])
     end
+    it"deveria devolver un arreglo "do
+        expect(Cambio(19)).to eq([19])
+    end
 end
